70a9315ec65dd4ef60316550e3b186e37dbab4dc
Prathmesh Nikam <118107698+prath457@users.noreply.github.com>
2025-12-31 12:36:11 +0530

    Add Hello World and arithmetic operations programs

--- DAY1.java
+++ DAY1.java
@@ -1,1 +1,23 @@
+✅ 1. Program To Print Hello World
 
+  public class Hello World{
+  public static void main(String args[]){
+  System.out.println("Hello World!");
+  }
+}
+
+✅ 2. Perform Arithmetic Operations on Two Numbers
+
+  public class Operations{
+  public static void main(String args[]){
+  int a = 10, b = 5;
+  System.out.println("Addition: " + (a + b));
+  System.out.println("Subtraction: " + (a - b));
+  System.out.println("Multplication: " + (a * b));
+  System.out.println("Devision: " + (a / b));
+  System.out.println("Module: " + (a % b));
+  }
+}
+                                  
+
+  
