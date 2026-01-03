0df03c2143943090deb5d86bdfda2fada1ccdbd9
Prathmesh Nikam <118107698+prath457@users.noreply.github.com>
2026-01-03 12:34:22 +0530

    Day1.java

--- DAY1.java
+++ DAY1.java
@@ -1,4 +1,4 @@
-✅ 1. Program To Print Hello World
+1. Program To Print Hello World
 
   public class Hello World{
   public static void main(String args[]){
@@ -6,7 +6,7 @@
   }
 }
 
-✅ 2. Perform Arithmetic Operations on Two Numbers
+ 2. Perform Arithmetic Operations on Two Numbers
 
   public class Operations{
   public static void main(String args[]){
@@ -19,5 +19,69 @@
   }
 }
                                   
+3. Accept Values from User and Display Them
 
-  
+  import java.util.Scanner;
+public class acceptname{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter a number: ");
+    int num = sc.nextInt();
+
+    System.out.println("Enter a name: ");
+    String name = sc.name();
+
+    System.out.println("number" + num);
+    System.out.println("Name" + name);
+  }
+}
+
+4. Accept 5 Subjects Marks, Calculate Total and Average
+
+  import java.util.Scanner;
+  public class average{
+    public static void main(String args[]){
+      Scanner sc = new Scanner(System.in);
+
+      System.out.println("Enter marks of Subject1: ");
+      int m1 = sc.nextInt();
+      System.out.println("Enter marks of Subject2: ");
+      int m2 = sc.nextInt();
+      System.out.println("Enter marks of Subject3: ");
+      int m3 = sc.nextInt();
+      System.out.println("Enter marks of Subject4: ");
+      int m4 = sc.nextInt();
+      System.out.println("Enter marks of Subject5: ");
+      int m5 = sc.nextInt();
+
+      int total= m1 + m2 + m3 + m4 + m5;
+      float average = total/5f;
+
+      System.out.println("Total marks= " + total);
+      System.out.println("average marks= " + average);
+    }
+  }
+
+5. Calculate Square and Square Root of a Number
+
+  import java.util.Scanner;
+
+public class SquareRoot {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        double num = sc.nextDouble();
+
+        double square = num * num;
+        double sqrt = Math.sqrt(num);
+
+        System.out.println("Square: " + square);
+        System.out.println("Square Root: " + sqrt);
+    }
+}
+
+      
+      
+      
