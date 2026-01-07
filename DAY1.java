393b7b4fa9c66f983d66bdd5e5d2470ed20879c1
Prathmesh Nikam <118107698+prath457@users.noreply.github.com>
2026-01-07 21:03:46 +0530

    update Day1.java
    Added a new class to swap two numbers using a temporary variable.

--- DAY1.java
+++ DAY1.java
@@ -81,6 +81,21 @@ public class SquareRoot {
         System.out.println("Square Root: " + sqrt);
     }
 }
+  
+6. Swap 2 Numbers With Temporary Variable
+  
+public class SwapWithTemp {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        System.out.println("Before Swap: a = " + a + ", b = " + b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("After Swap: a = " + a + ", b = " + b);
+    }
+}
 
       
       
