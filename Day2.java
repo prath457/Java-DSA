b4a15947979094bf2f2326fa1761e4adb4fa4bef
Prathmesh Nikam <118107698+prath457@users.noreply.github.com>
2026-02-23 20:39:01 +0530

    update Day2.java

--- Day2.java
+++ Day2.java
@@ -12,3 +12,18 @@ import java.util.*;
           System.out.println(marks[2]);
       }
 }
+
+OR
+
+import java.util.*;
+  public class array {
+    public static void main(String args[]);
+        int marks[]= new int[3];
+              marks[0]=98;
+              marks[1]=97;
+              marks[2]=95;
+         for(i=0;i<3;i++) {
+    System.out.println(marks[i]);
+      }
+   }
+}
